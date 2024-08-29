e5b2245d7fce1684332daa4d449f43b274373681
Umar22222 <asadovumar222@gmail.com>
2024-08-29 17:26:31 +0500

    .

---
+++ js/app.js
@@ -0,0 +1,155 @@
+{
+    // 1 - misol
+    // let a = +prompt("sonni kiriting ?");
+
+    // if (a > 0) {
+    //   a++;
+    //   console.log(a);
+    // } else {
+    //   console.log(a);
+    // }
+  }
+
+  {
+    // 2- misol
+    // let a = +prompt("sonni kiriting ?");
+
+    // if (a > 0) {
+    //   a++;
+    //   console.log(a);
+    // } else {
+    //   a -= 2;
+    //   console.log(a);
+    // }
+  }
+  
+  {
+    // 3 - misol
+    // let a = +prompt("sonni kiriting ?");
+    
+    // if (a > 0) {
+    //   a++;
+    //   console.log(a);
+    // } else if (a < 0) {
+    //   a -= 2;
+    //   console.log(a);
+    // } else {
+    //   a = 10;
+    //   console.log(a);
+    // }
+  }
+  {
+      // 4 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+    // let d = +prompt("sonni kiriting ?");
+    // let musbat = 0;
+
+    // if (a > 0) {
+    //   musbat++;
+    // }
+    // if (b > 0) {
+    //   musbat++;
+    // }
+    // if (d > 0) {
+    //   musbat++;
+    // }
+    // console.log(`${musbat} ta musbat son bor`);
+  }
+
+  {
+      // 5 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+    // let d = +prompt("sonni kiriting ?");
+    // let musbat = 0;
+    // let manfiy = 0;
+
+    // if (a > 0) {
+    //   musbat++;
+    // } else {
+    //   manfiy++;
+    // }
+    // if (b > 0) {
+    //   musbat++;
+    // } else {
+    //   manfiy++;
+    // }
+    // if (d > 0) {
+    //   musbat++;
+    // } else {
+    //   manfiy++;
+    // }
+    // console.log(
+    //   `${musbat} ta musbat son va ${manfiy} ta manfiy son bor`);
+  }
+
+  {
+      // 6 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+
+    // if (a > b) {
+    //   console.log(a);
+    // } else {
+    //   console.log(b);
+    // }
+  }
+
+  {
+      // 7 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+
+    // if (a > b) {
+    //   console.log(2);
+    // } else {
+    //   console.log(1);
+    // }
+  }
+  {
+      // 8 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+
+    // if (a > b) {
+    //   console.log(a);
+    //   console.log(b);
+    // } else {
+    //   console.log(b);
+    //   console.log(a);
+    // }
+  }
+
+  {
+    // 9 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+    // let c;
+    
+    // if (a > b) {
+    //   c = b;
+    //   b = a;
+    //   a = c;
+    //   console.log(b);
+    //   console.log(a);
+    // }
+  }
+  
+  {
+    //  10 - misol
+    // let a = +prompt("sonni kiriting ?");
+    // let b = +prompt("sonni kiriting ?");
+    // let c = a + b;
+    // if (a === b) {
+    //     a = c;
+    //     b = c;
+    //     console.log(a);
+    //     console.log(b);
+    // } else {
+    //     a = c;
+    //     b = c;
+    //     console.log(a);
+    //     console.log(b);
+    // }
+  }
